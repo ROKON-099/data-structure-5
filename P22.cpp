fae0ff9057d45136982437c96734f7b034247cde
Rokon007 <mdrokonuzzaman099@gmail.com>
2026-03-10 20:43:40 +0600

    2nd commit

---
+++ P22.cpp
@@ -0,0 +1,20 @@
+ #include<iostream>
+ using namespace std;
+ int main(){
+ int x=5;
+ int*p=&x;
+
+  cout << " value of Num:" <<x <<endl;
+  cout << "address of this num:" << &x<<endl;
+
+  cout << "Value of Pointer:" <<*p <<endl;
+  cout << " address of pointer:" <<p <<endl;
+
+
+
+
+
+
+
+
+ }
